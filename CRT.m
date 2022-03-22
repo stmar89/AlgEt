4de1f297df5bd0cfce2c2f5b92e7c2e5943c9d05
stmar89 <stefano.marseglia89@gmail.com>
2022-03-22 10:52:32 -0400

    changes to CRT

--- CRT.m
+++ CRT.m
@@ -25,33 +25,32 @@ import "Ord.m" : crQZ , crZQ , Columns , hnf , MatrixAtoQ , MatrixAtoZ , MatrixQ
 CRT_data_order:=function(S)
     if not assigned S`CRT_data then
         K:=Algebra(S);
-        Zbasis_S:=ZBasis(S);
-        //I need to modify the ZBasis(S) in a way that One(K) is the first element of Zbasis_S
-        // it is a bit weird, but the output is asserted...
-        pos:=Position(Zbasis_S,One(K));
+        zb:=ZBasis(S); 
+        //I need to modify the ZBasis(S) in a way that One(K) is the first element
+        pos:=Position(zb,One(K));
         if pos ne 1 then
             vprintf CRT,2 : "1 is not in the first position of ZBasis(S)\n";
             if pos eq 0 then //One(K) not in Zbasis_S
-                // this should happen only if one of the orthogonal idempotens is there.
-                // there is no harm at replacing any orthogonal idempotent with One(K).
                 vprintf CRT,2 : "1 is not in ZBasis(S)\n";
-                coord:=AbsoluteCoordinates([One(K)],Zbasis_S)[1];
+                coord:=AbsoluteCoordinates([One(K)],zb)[1];
+                // since One(K) is in S, and there exists a Z-Basis of S containing One(K), then there must be a coefficient of coord that is a unit in Z, i.e. 1 or -1.
+                // the element for which this occurs can be replaced by One(K)
                 pos:=Position(coord,1);
                 if pos eq 0 then
                     pos:=Position(coord,-1);
                 end if;
                 assert pos ne 0;
             end if; 
-            //replacing Zbasis_S[pos] with One(K) since they generate the same Z-Span
-            temp:=Zbasis_S[1];
-            Zbasis_S[1]:=One(K);
-            Zbasis_S[pos]:=temp;
+            //replacing zb[pos] with One(K) since they generate the same Z-Span
+            temp:=zb[1];
+            zb[1]:=One(K);
         end if;
-        vprintf CRT,2 : "Not using MinimalInteger\n";
-        M:=MatrixAtoQ(Zbasis_S);
-        // I was expecting this Matrix inversion to be the most expensive one. But apparently it is not.
+        assert2 Order(zb) eq S;
+        M:=MatrixAtoQ(zb);
         Minv:=M^-1;
-        S`CRT_data:=<Zbasis_S,Minv>;
+    pow_a:=[ i gt 1 select Self(i-1)*a else One(A) : i in [1..#coeff]];
+    //Self makes it much more efficient
+        S`CRT_data:=<zb,Minv>;
     end if;
     return Explode(S`CRT_data);
 end function;
@@ -59,6 +58,7 @@ end function;
 CRT_data_ideal:=function(I)
     if not assigned I`CRT_data then
         _,Minv:=CRT_data_order(Order(I)); 
+        vprintf CRT,2 : "CRT_data_ideal output = %o\n",(MatrixAtoQ(ZBasis(I))*Minv);
         I`CRT_data:=crQZ(MatrixAtoQ(ZBasis(I))*Minv);
     end if;
     return I`CRT_data;
@@ -66,10 +66,11 @@ end function;
 
 intrinsic ChineseRemainderTheorem(I::AlgEtIdl,J::AlgEtIdl,a::AlgEtElt,b::AlgEtElt)-> AlgEtElt
 {Given two coprime ideals I and J of S, two elements a,b in S, finds e such that (e-a) in I and (e-b) in J.}
-    require IsCoprime(I,J) : "the ideals must be coprime";
     S:=Order(I);
+    vprintf CRT,2 : "S :=%o;\nI := %o;\nJ := %o;\n\/\/[a,b] =\nelts := %o;\n",PrintSeqAlgEtElt(ZBasis(S)),PrintSeqAlgEtElt(ZBasis(I)),PrintSeqAlgEtElt(ZBasis(J)),PrintSeqAlgEtElt([a,b]); 
     require a in S and b in S:"the elements must lie in order of definition of the ideals";
     require S eq Order(J): "the ideals must be of the same order";
+    require IsCoprime(I,J) : "the ideals must be coprime";
     I_min:=MinimalInteger(I);
     J_min:=MinimalInteger(J);
     g,c1,d1:=XGCD(I_min,J_min);
@@ -77,6 +78,7 @@ intrinsic ChineseRemainderTheorem(I::AlgEtIdl,J::AlgEtIdl,a::AlgEtElt,b::AlgEtEl
         K:=Algebra(S);
         n:=AbsoluteDimension(K);
         Zbasis_S,Minv:=CRT_data_order(S);
+        vprintf CRT,2 : "ZBasis_S := %o;\n",PrintSeqAlgEtElt(Zbasis_S);
         A:=CRT_data_ideal(I);
         B:=CRT_data_ideal(J);
         C:=VerticalJoin(A,B);
@@ -100,6 +102,7 @@ intrinsic ChineseRemainderTheorem(I::AlgEtIdl,J::AlgEtIdl,a::AlgEtElt,b::AlgEtEl
         d:=d1*J_min;
     end if;
     e:=a*d+b*c;
+    vprintf CRT,2 : "e := %o;\n",PrintSeqAlgEtElt([e])[1];
     assert e-a in I;
     assert e-b in J;
     return e;
@@ -108,17 +111,13 @@ end intrinsic;
 
 /* TEST
 
-    Attach("~/packages_github/AlgEt/AlgEt.m");
-    Attach("~/packages_github/AlgEt/Elt.m");
-    Attach("~/packages_github/AlgEt/Ord.m");
-    Attach("~/packages_github/AlgEt/TraceNorm.m");
-    Attach("~/packages_github/AlgEt/Idl.m");
-    Attach("~/packages_github/AlgEt/WkTesting.m");
-    Attach("~/packages_github/AlgEt/FactPrimes.m");
-    Attach("~/packages_github/AlgEt/CRT.m");
-
-    SetVerbose("CRT",1);
+    AttachSpec("~/packages_github/AlgEt/spec");
+    SetVerbose("CRT",2);
     SetAssertions(1);
+
+    ////////////////
+    //Simple extension of Q
+    ///////////////
 
     _<x>:=PolynomialRing(Integers());
     f:=(x^8+16)*(x^8+81);
@@ -146,7 +145,11 @@ end intrinsic;
     SetProfile(false);
     ProfilePrintByTotalTime(ProfileGraph());
     Cputime(t0);
-
+    
+    //////////////////
+    //Relative setting
+    /////////////////
+    // issues here :-(
 
     _<x>:=PolynomialRing(Integers());
     K:=NumberField(x^2-5);

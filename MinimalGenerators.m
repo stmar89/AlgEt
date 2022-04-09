c095bfbe82bdef2869d7fbcebfddce32e4fecb51
stmar89 <stefano.marseglia89@gmail.com>
2022-04-09 04:38:00 -0400

    added a Lemma in MinimalGenerators.m. to be implemented

--- MinimalGenerators.m
+++ MinimalGenerators.m
@@ -32,31 +32,38 @@ quo_frac_idl:=function(I,zbJ)
 end function;
 
 intrinsic TwoGeneratingSet(I::AlgEtIdl)
-{A procedure that given an invertible ideal I creates produces 2 elemetns that generate I. If I is known to be principal nothing is done. }
+{A procedure that given an invertible ideal I put in the attibute I`Generators two non-zerodivisors in I that generate I. If I is known to be principal, that is I`Generators consists of one single element, nothing is done.}
     require IsInvertible(I) : "the ideal must be invertible";
     if #Generators(I) gt 2 then
         if assigned I`IsIntegral and IsIntegral(I) then
             a:=Algebra(I)!MinimalInteger(I);
         else
-            a:=Random(I);
+            a:=Random(I : ZeroDivisorsAllowed:=false );
         end if;
         S:=Order(I);
         Q,q:=quo_frac_idl(I,[a*z : z in ZBasis(S)]);
         if #Q eq 0 then
-            I`Generators:=[a]; //the ideal is principalA
+            I`Generators:=[a]; //the ideal is principal
         else
             repeat
                 repeat
                     b:=Random(Q);
                 until b ne Zero(Q);
                 b:=b@@q;
+                assert not IsZeroDivisor(b);
             until I eq Ideal(S,[a,b]);
         end if;
         I`Generators:=[a,b];
     end if;
 end intrinsic;
 
-
+// Lemma:
+// Let I be an invertible fractional S-ideal generated globally by I=aS+bS.
+// For every integer N, we have I^N=a^NS+b^NS.
+// Proof: we will show it locally at every prime. Since I is invertible, then it is locally principal.
+// Pick a prime P. Then I_P=(a/1)S_P or I_P=(b/1)S_P. wlog assume the first. Then I_P^N=(a^N/1)S_P. QED.
+//
+// Remark: this trick does not work for product of invertible ideals I and J. One can have I_P1=(a) J_P1=(c) but I_P2=(a), J_P2=(d) so one cannot avoid the mixed products.
 
 
 /* TEST

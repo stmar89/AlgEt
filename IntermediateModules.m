a8ae771cf3db2316d059adeec0fea293f863685d
Stefano Marseglia <stefano.marseglia89@gmail.com>
2022-08-24 11:00:35 -0400

    updated diary and running times

--- IntermediateModules.m
+++ IntermediateModules.m
@@ -33,7 +33,7 @@ intrinsic MinimalIntermediateModules(I::AlgEtMod,J::AlgEtMod)->SetIndx[AlgEtMod]
         // The first case occurs if p does not divide the order of N.
         // The second occurs if p divides the order of N.
         // QED
-        for p in PrimeDivisors(Index(I,J)) do
+        for p in PrimeDivisors(#Q) do
             mp:=hom<Q->Q | x:->p*x>;
             Qp:=Kernel(mp);
             rk:=Ngens(Qp);
@@ -52,6 +52,43 @@ intrinsic MinimalIntermediateModules(I::AlgEtMod,J::AlgEtMod)->SetIndx[AlgEtMod]
                                     [(Q!(Qp!Eltseq(Qp_Rmod!b)))@@q : b in Basis(min)]) : min in min_Rmod @};
         end for;
         return min_mods;
+    end if;
+end intrinsic;
+
+intrinsic MaximalIntermediateModules(I::AlgEtMod,J::AlgEtMod)->SetIndx[AlgEtMod]
+{ Given S-modules J subset I, returns the maximal (with respect to inclusion) S-modules K such that J subset K subset I. }
+    assert2 J subset I; // "the ideal J needs to be inside I";
+    S:=Order(I);
+    assert2 S eq Order(J); // "The ideals must be over the same order";
+    if J eq I then 
+        return {@ @};
+    else
+        // If I c K c I is maximal then I/K is a simple module, that is, isomorphic to S/P for some prime P of S.
+        // In particular, if p is the characteristic of S/P, then p*I c K c I.
+        // Note that I/pI is an Fp-vector space.
+        _,m:=UniverseAlgebra(I);
+        gens_J_over_S:=Generators(J);
+        Q,q:=Quotient(I,J);
+        max_ideals:={@ @};
+        for p in PrimeDivisors(#Q) do
+            mp:=[ p*(Q.j) : j in [1..Ngens(Q)] ];
+            Qp,qp:=quo<Q|mp>;
+            rk:=Ngens(Qp);
+            assert #Qp eq p^rk;
+            Fp:=FiniteField(p);
+            // Note: Qp = Fp^rk as a vector space
+            // So its S-module structure can be realized over Fp.
+            // More precisely, given generators ai of S over Z, 
+            // to find the (minimal) sub-S-modules, it is enough to look at Qp as module over Fp[ai]
+            // In matrices we store the matrix representations of the action of the ZBasis of S on Qp
+            matrices:=[Matrix(Fp,[ Eltseq(qp(q(m(zS)*(Qp.j@@qp@@q)))) : j in [1..Ngens(Qp)]]) : zS in ZBasis(S) ];
+            matrices:=Setseq(Seqset(matrices)); //possibly there are repetiions.
+            Qp_Rmod:=RModule(matrices);
+            max_Rmod:=MaximalSubmodules(Qp_Rmod);
+            max_ideals join:={@ Module(S,m, gens_J_over_S cat 
+                                    [(Qp!Eltseq(Qp_Rmod!b))@@qp@@q : b in Basis(max)]) : max in max_Rmod @};
+        end for;
+        return max_ideals;
     end if;
 end intrinsic;
 
@@ -97,6 +134,33 @@ intrinsic IntermediateModulesWithPrescribedExtension(I::AlgEtMod,J::AlgEtMod,O::
     end while;
     return output;
 end intrinsic;
+
+intrinsic IntermediateModulesWithTrivialExtension(I::AlgEtMod,J::AlgEtMod,O::AlgEtOrd)->SetIndx[AlgEtMod]
+{ Given S-modules J subset I, and overorder O of S and a fixed O-module M subset J, it returns all the S-modules N such that I subset N subset J and NO=M.
+  They are produced recursively using from the minimal ones }
+    V,m:=UniverseAlgebra(I);
+    VJ,mJ:=UniverseAlgebra(I);
+    S:=Order(I);
+    require S eq Order(J) : "The modules must be over the same order";
+    require V eq VJ and forall{b : b in Basis(Algebra(S)) | m(b) eq mJ(b)} : "the modules are not compatible";
+    require J subset I : "The module J needs to be inside I";
+    require S subset O : "The order O must be bigger than the order of I"; 
+    IO:=O!!I;
+    require S!!IO eq I : "I must be an O-module";
+    queue:={@ I @};
+    output:={@ J @};
+    done:={@ @};
+    while #queue gt 0 do
+        pot_new:=&join[MaximalIntermediateModules(elt,J) : elt in queue ];
+        pot_new:={@ K : K in pot_new | O!!K eq IO @}; // only the ones with trivial extension.
+        output join:={@ K : K in pot_new | not K in done and O!!K eq IO @};
+        done join:=queue;
+        // Note: if O!!K is not IO then every submodule of K will also not have trivial extension. Hence such K's can be excluded from the recursion.
+        queue := pot_new diff done;
+    end while;
+    return output;
+end intrinsic;
+
 
 /* CONTINUE FROM HERE
 intrinsic IntermediateIdealsWithPrescribedMultiplicatorRing(I::AlgEtIdl,J::AlgEtIdl)->SetIndx[AlgEtIdl]
@@ -155,13 +219,14 @@ end intrinsic;
     Q,q:=Quotient(NE,N2);
     #Q;
     time #MinimalIntermediateModules(NE,N2); // 0.1 secs
-    time l:=IntermediateModules(NE,N2); #l; // 10 secs
-    time l2:=IntermediateModulesWithPrescribedExtension(NE,N2,O,O!!NE); ~12 secs
+    time l:=IntermediateModules(NE,N2); #l; // 5 secs
+    time l2:=IntermediateModulesWithPrescribedExtension(NE,N2,O,O!!NE); // ~5 secs
+    time l3:=IntermediateModulesWithTrivialExtension(NE,N2,O); // ~1 secs
     NEO:=O!!NE;
     assert #l2 eq #[ M : M in l | O!!M eq NEO ];
+    assert #l2 eq #l3;
 
     // a much bigger test!
-
     AttachSpec("~/packages_github/AlgEt/spec");
     Attach("~/packages_github/AlgEt/Modules.m");
     Attach("~/packages_github/AlgEt/IntermediateModules.m");
@@ -203,9 +268,10 @@ end intrinsic;
     // the following works for this exmaple, not in general.
     Mff:=Module(R,m,<ff_prod[1] : i in [1..2]>);
     gensMff:=Generators(Mff);
-    time l0:=IntermediateModulesWithPrescribedExtension(MO,Mff,O,MOO);
+    time l0:=IntermediateModulesWithTrivialExtension(MO,Mff,O);
     time l1:=IntermediateModules(MO,Mff);
-    time l2:=[ I : I in candidates | O!!I eq MO ];
+    time l2:=[ I : I in l1 | O!!I eq MO ];
     assert #l2 eq #l0;
+    "then do the julia thing...";
 
 */

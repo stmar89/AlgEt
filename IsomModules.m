fad0081d5462ee03eb8e2823a507217aa13f967e
Stefano Marseglia <stefano.marseglia89@gmail.com>
2022-10-13 08:10:14 -0400

    implemented improvement in IsomModules

--- IsomModules.m
+++ IsomModules.m
@@ -97,12 +97,40 @@ In the latter case, the Method should be of the form "julia path/to/AlgEt/", or 
     Mff:=Module(R,m,<ff_prod[Index(Knfpoly,Vnfpoly[i])] : i in [1..#Vnf]>);
     vprint IsomModules,1 : "candidates:";
     vtime IsomModules,1 : candidates:=IntermediateModulesWithTrivialExtension(MO,Mff,O);
+    // candidates contains all representatives of the isomorphism classes of modules whose O-extension is the trivial Steinitz class.
     vprint IsomModules,1 : #candidates;
+    classes:=[]; // the final output
+
+    // now we sieve out a minimal set of representatives of the isomorphism classes from candidates.
+    if Method eq "Magma" then
+        classes_O:=[];
+        for M in candidates do
+            if not exists{ N : N in classes_O | IsIsomorphic(M,N : Method:=Method) } then
+                Append(~classes_O,M);
+            end if;
+        end for;
+    else //Method eq "julia ...."
+        ID:=&cat[ Sprint(Random(0,9)) : i in [1..20] ];
+        file:="tmp_" cat ID cat ".txt";
+        str:="[\n";
+        for i->I in candidates do
+            mat:=Matrix(AbsoluteCoordinates([pi*z : z in ZBasis(I)],ZBasis(I)));
+            assert MinimalPolynomial(mat) eq min_poly;
+            assert CharacteristicPolynomial(mat) eq char_poly;
+            str cat:=Sprintf("%o,\n", Eltseq(mat));
+        end for;
+        str:=Prune(Prune(str)) cat "\n]\n";
+        fprintf file, "%o",str;
+        indices_of_classes_O:=eval(Pipe(Method cat "IsIsomorphic_julia_script.jl " cat file,""));
+        vprintf IsomModules,1 : "indices_of_classes_k = %o\n",indices_of_classes_O;
+        classes_O:=[candidates[i] : i in indices_of_classes_O];
+    end if;
+
+
     PO,pO:=PicardGroup(O);
-    classes:=[];
     for g in PO do
         if g eq Zero(PO) then
-            candidates_k:=candidates; 
+            classes_k:=classes_O; 
         else
             _,Ik:=CoprimeRepresentative(pO(g),ff); // Ik+ff=O, Ik cap ff = Ik*ff
             test,Ik_prod:=IsProductOfIdeals(Ik);
@@ -120,40 +148,104 @@ In the latter case, the Method should be of the form "julia path/to/AlgEt/", or 
             // ik induces the isomorphism between (O1^s1 + ... + On^sn)/(f1^s1 + ... + fn^sn),
             // and (O1^(s1-1)+I1 + ... + On^(sn-1)+In)/(f1^(s1-1)+f1I1 + ... + fn^(sn-1)+fnIn),
             // where Ik = I1 + .... + In.
-            candidates_k:=[ Module(R,m, [ik(z) : z in ZBasis(M)] cat zbMffIk ) : M in candidates];
-        end if;
-        if Method eq "Magma" then
-            classes_k:=[];
-            for M in candidates_k do
-                if not exists{ N : N in classes_k | IsIsomorphic(M,N : Method:=Method) } then
-                    Append(~classes_k,M);
-                end if;
-            end for;
-        else //Method eq "julia ...."
-            ID:=&cat[ Sprint(Random(0,9)) : i in [1..20] ];
-            file:="tmp_" cat ID cat ".txt";
-            str:="[\n";
-            for i->I in candidates do
-                mat:=Matrix(AbsoluteCoordinates([pi*z : z in ZBasis(I)],ZBasis(I)));
-                assert MinimalPolynomial(mat) eq min_poly;
-                assert CharacteristicPolynomial(mat) eq char_poly;
-                str cat:=Sprintf("%o,\n", Eltseq(mat));
-            end for;
-            str:=Prune(Prune(str)) cat "\n]\n";
-            fprintf file, "%o",str;
-            indices_of_classes_k:=eval(Pipe(Method cat "IsIsomorphic_julia_script.jl " cat file,""));
-            vprintf IsomModules,1 : "indices_of_classes_k = %o\n",indices_of_classes_k;
-            classes_k:=[candidates_k[i] : i in indices_of_classes_k];
+            classes_k:=[ Module(R,m, [ik(z) : z in ZBasis(M)] cat zbMffIk ) : M in classes_O];
         end if;
         Append(~classes,classes_k);
     end for;
-    assert forall{ c : c in classes | #c eq #classes[1] }; // CONJECTURE THEY ALL CONTAIN THE SAME NUMBER OF CLASSES
     classes:=&cat(classes);
     return classes;
 end intrinsic;
 
+// OLD VERSION, where we produce the candidates for each Steinitz decomposition.
+// intrinsic IsomorphismClasses(R::AlgEtOrd,m::Map : Method:="Magma") -> SeqEnum[AlgEtMod]
+// {Given an order R in some AlgEt K, where K acts on some V, by m:K->V, returns representatives of hte isomorphism classes of the S-module lattices in V.
+// The vararg Method allows to choose if the isomorphism testing is done with "Magma", very slow, or with the Hecke/Nemo package for julia, which is much faster.
+// In the latter case, the Method should be of the form "julia path/to/AlgEt/", or if Hecke/Nemo has been built,"julia -J /tmp/Hecke.so ~/path/to/AlgEt/" (the ".so" might be different according to your SO. See the documentation of Hecke.Build()).}
+//     require Method eq "Magma" or Method[1..5] eq "julia" : "Method should be Magma or julia or a string of julia command";
+//     V:=Codomain(m);
+//     K:=Domain(m);
+//     pi:=PrimitiveElement(K);
+//     if not pi in R then
+//         pi:=pi*Exponent(Quotient(pi*R + OneIdeal(R),OneIdeal(R)));
+//     end if;
+//     pi:=m(pi);
+//     O:=MaximalOrder(K);
+//     ff:=Conductor(R);
+//     Vnf:=NumberFields(V);
+//     Knfpoly:=[ DefiningPolynomial(L) : L in NumberFields(K) ];
+//     Vnfpoly:=[ DefiningPolynomial(L) : L in Vnf ];
+//     MO:=Module(R,m,<1*MaximalOrder(Vnf[i]) : i in [1..#Vnf]>);
+//     mat:=Matrix(AbsoluteCoordinates([pi*z : z in ZBasis(MO)],ZBasis(MO)));
+//     min_poly:=MinimalPolynomial(mat);
+//     char_poly:=CharacteristicPolynomial(mat);
+//     ff:=O!!Conductor(R);
+//     test,ff_prod:=IsProductOfIdeals(ff);
+//     assert test;
+//     ind:=[ #Vnfpoly+1 - Index(Reverse(Vnfpoly),fK)  : fK in Knfpoly ]; // last occurence of each number field from the dec of K 
+//                                                                        // in the decomposition of V
+//     Mff:=Module(R,m,<ff_prod[Index(Knfpoly,Vnfpoly[i])] : i in [1..#Vnf]>);
+//     vprint IsomModules,1 : "candidates:";
+//     vtime IsomModules,1 : candidates:=IntermediateModulesWithTrivialExtension(MO,Mff,O);
+//     vprint IsomModules,1 : #candidates;
+//     PO,pO:=PicardGroup(O);
+//     classes:=[];
+//     for g in PO do
+//         if g eq Zero(PO) then
+//             candidates_k:=candidates; 
+//         else
+//             _,Ik:=CoprimeRepresentative(pO(g),ff); // Ik+ff=O, Ik cap ff = Ik*ff
+//             test,Ik_prod:=IsProductOfIdeals(Ik);
+//             assert test;
+//             MffIk:=Module(R,m,<i in ind select 
+//                                             ff_prod[Index(Knfpoly,Vnfpoly[i])] 
+//                                         else ff_prod[Index(Knfpoly,Vnfpoly[i])]*Ik_prod[Index(Knfpoly,Vnfpoly[i])] 
+//                                                 : i in [1..#Vnf]>); // MffIk = f1^(s1-1)+f1I1 + ... + fn^(sn-1)+fnIn
+//             zbMffIk:=ZBasis(MffIk);
+//             e:=ChineseRemainderTheorem(ff,Ik,One(K),Zero(K)); // e in Ik, e-1 in ff.
+//             assert not IsZeroDivisor(e);
+//             e:=Components(e);
+//             e:=V!< i in ind select e[Index(Knfpoly,Vnfpoly[i])] else Vnf[i] ! 1 : i in [1..#Vnf] >;
+//             ik:=map<V->V | x:->x*e >;
+//             // ik induces the isomorphism between (O1^s1 + ... + On^sn)/(f1^s1 + ... + fn^sn),
+//             // and (O1^(s1-1)+I1 + ... + On^(sn-1)+In)/(f1^(s1-1)+f1I1 + ... + fn^(sn-1)+fnIn),
+//             // where Ik = I1 + .... + In.
+//             candidates_k:=[ Module(R,m, [ik(z) : z in ZBasis(M)] cat zbMffIk ) : M in candidates];
+//         end if;
+//         if Method eq "Magma" then
+//             classes_k:=[];
+//             for M in candidates_k do
+//                 if not exists{ N : N in classes_k | IsIsomorphic(M,N : Method:=Method) } then
+//                     Append(~classes_k,M);
+//                 end if;
+//             end for;
+//         else //Method eq "julia ...."
+//             ID:=&cat[ Sprint(Random(0,9)) : i in [1..20] ];
+//             file:="tmp_" cat ID cat ".txt";
+//             str:="[\n";
+//             for i->I in candidates_k do
+//                 mat:=Matrix(AbsoluteCoordinates([pi*z : z in ZBasis(I)],ZBasis(I)));
+//                 assert MinimalPolynomial(mat) eq min_poly;
+//                 assert CharacteristicPolynomial(mat) eq char_poly;
+//                 str cat:=Sprintf("%o,\n", Eltseq(mat));
+//             end for;
+//             str:=Prune(Prune(str)) cat "\n]\n";
+//             fprintf file, "%o",str;
+//             indices_of_classes_k:=eval(Pipe(Method cat "IsIsomorphic_julia_script.jl " cat file,""));
+//             vprintf IsomModules,1 : "indices_of_classes_k = %o\n",indices_of_classes_k;
+//             classes_k:=[candidates_k[i] : i in indices_of_classes_k];
+//         end if;
+//         Append(~classes,classes_k);
+//     end for;
+//     assert forall{ c : c in classes | #c eq #classes[1] }; // CONJECTURE THEY ALL CONTAIN THE SAME NUMBER OF CLASSES
+//     classes:=&cat(classes);
+//     return classes;
+// end intrinsic;
+
 /* TEST
-  
+    // ##########
+    // Fast Tests
+    // ##########
+
     AttachSpec("~/packages_github/AlgEt/spec");
     Attach("~/packages_github/AlgEt/Modules.m");
     Attach("~/packages_github/AlgEt/IntermediateModules.m");
@@ -166,9 +258,10 @@ end intrinsic;
     pi:=PrimitiveElement(K);
     R:=Order([7*pi]);
     m:=NaturalAction(K,K);
-    time classes:=IsomorphismClasses(R,m : Method:="Magma");
-    time classes:=IsomorphismClasses(R,m : Method:="julia -J /tmp/Hecke.so ~/packages_github/AlgEt/"); // this is the ICM
     time icm:=ICM(R);
+    time classes:=IsomorphismClasses(R,m : Method:="Magma");
+    assert #classes eq #icm; // Since R is Bass, the ICM is Pic(R) cup Pic(O)
+    time classes:=IsomorphismClasses(R,m : Method:="julia -J /tmp/Hecke.so ~/packages_github/AlgEt/"); // this is the ICM
     assert #classes eq #icm; // Since R is Bass, the ICM is Pic(R) cup Pic(O)
 
     // Pic(O) is non trivial
@@ -192,14 +285,9 @@ end intrinsic;
     V:=EtaleAlgebra(&cat[nf : i in [1..2]]);
     m:=NaturalAction(K,V);
     time classes:=IsomorphismClasses(R,m : Method:="Magma");
+    assert #classes eq 6; // because R is Bass 
     time classes:=IsomorphismClasses(R,m : Method:="julia -J /tmp/Hecke.so ~/packages_github/AlgEt/");
     assert #classes eq 6; // because R is Bass 
-    // THERE IS A BUG in the julia code :  V = K^3
-    V:=EtaleAlgebra(&cat[nf : i in [1..3]]);
-    m:=NaturalAction(K,V);
-    time classes:=IsomorphismClasses(R,m : Method:="Magma");
-    time classes:=IsomorphismClasses(R,m : Method:="julia -J /tmp/Hecke.so ~/packages_github/AlgEt/");
-    assert #classes eq 8; // because R is Bass 
 
     AttachSpec("~/packages_github/AlgEt/spec");
     Attach("~/packages_github/AlgEt/Modules.m");
@@ -244,7 +332,45 @@ end intrinsic;
     time icm:=ICM(R);
     assert #classes eq #icm; // Since R is Bass, the ICM is Pic(R) cup Pic(O)
 
-    // THIS LAST TEST IS VERY SLOW. Computing the candidates (1774) takes 470 secs. The julia routine to sieve out the representatives does not finish in one night.
+    // ########## 
+    // Slow Tests
+    // #########
+
+    // Pic(O) is non trivial
+    AttachSpec("~/packages_github/AlgEt/spec");
+    Attach("~/packages_github/AlgEt/Modules.m");
+    Attach("~/packages_github/AlgEt/IntermediateModules.m");
+    Attach("~/packages_github/AlgEt/IsomModules.m");
+    SetVerbose("IsomModules",1);
+    _<x>:=PolynomialRing(Integers()); 
+    g:=x^2+15;
+    K:=EtaleAlgebra(g);
+    nf:=NumberFields(K);
+    pi:=PrimitiveElement(K);
+    R:=Order([pi]);
+    // V = K^3 this is expected to be considerably slower. Because of the bugs described below, I never got to the end.
+    // Currently there is a bug in the julia function which triggers some error. Tommy has it fixed. Need to check after update
+    // There is a bug also in the Isomorphism testing routing the Magma. It returns false, when it should be true. It has been reported, but I don't expect any quick action.
+    V:=EtaleAlgebra(&cat[nf : i in [1..3]]);
+    m:=NaturalAction(K,V);
+    //time classes:=IsomorphismClasses(R,m : Method:="Magma"); // 800 secs. Faster than expected.
+    //assert #classes eq 8; // because R is Bass. Note that the Magma method is bugged.
+    time classes:=IsomorphismClasses(R,m : Method:="julia -J /tmp/Hecke.so ~/packages_github/AlgEt/");
+    assert #classes eq 8; // because R is Bass 
+
+    AttachSpec("~/packages_github/AlgEt/spec");
+    Attach("~/packages_github/AlgEt/Modules.m");
+    Attach("~/packages_github/AlgEt/IntermediateModules.m");
+    Attach("~/packages_github/AlgEt/IsomModules.m");
+    SetVerbose("IsomModules",1);
+    _<x>:=PolynomialRing(Integers()); 
+    g:=x^6-x^5+2*x^4-2*x^3+4*x^2-4*x+8;
+    K:=EtaleAlgebra(g);
+    nf:=NumberFields(K);
+    q:=2;
+    pi:=PrimitiveElement(K);
+    R:=Order([pi,q/pi]);
+    // never finished so far. There are ~1770 candidates.
     // V = K^3
     V:=EtaleAlgebra(&cat[nf : i in [1..3]]);
     m:=NaturalAction(K,V);

2b84195faff14e489cd2e2d2cead8f05a050028b
Stefano Marseglia <stefano.marseglia89@gmail.com>
2022-12-08 12:00:46 -0500

    fixed HomsToC

--- Homs.m
+++ Homs.m
@@ -22,14 +22,12 @@ declare attributes AlgEt : HomsToC;
 intrinsic HomsToC(A::AlgEt : Precision:=30)->SeqEnum[Map]
 {returns Hom(A,\C) as a sequence of maps. The precision of \C is given by the optional parameter "Precision". Default value is 30}
     if not assigned A`HomsToC then
-        require Type(PrimeField(A)) eq "FldRat";
+        require Type(PrimeField(A)) eq  FldRat : "The algebra needs to be over Q";
         CC:=ComplexField(Precision);
         images:=function(x)
             return &cat[[CC ! z : z in Conjugates(y : Precision:=Precision)] :y in Components(x)];
         end function;
         maps:=< map< A -> CC | x:-> images(x)[k] > : k in [1..Dimension(A)] >;
-        f:=&*[DefiningPolynomial(L[1]) : L in Components(A)];
-        assert &and [ Abs(Evaluate(f,g(PrimitiveElement(A)))) lt 10^-10 : g in maps]; //the precision here is quite arbitrary...
         A`HomsToC:=maps;
     end if;
     return A`HomsToC;
@@ -43,6 +41,6 @@ end intrinsic;
     A:=EtaleAlgebra(f);
     homs:=HomsToC(A);
     a:=PrimitiveElement(A);
-    [ h(a) : h in homs ];
+    assert &and[ Abs(Evaluate(f,h(a))) lt 10^-20 : h in homs ];
 
 */

147b05a25e4e944f8534c778d5101c2b88841424
stmar89 <stefano.marseglia89@gmail.com>
2022-12-21 18:50:24 +0100

    test

--- ZBasisLLL.m
+++ ZBasisLLL.m
@@ -20,7 +20,7 @@ import "Ord.m" : MatrixAtoQ , MatrixQtoA;
 
 intrinsic ZBasisLLL(S::AlgEtOrd)
 {A procedure that replaces the ZBasis with an LLL-reduced one.}
-  if not assigned S`IsZBasisLLLReduced or not S`IsZBasisLLLReduced then
+  if (not assigned S`IsZBasisLLLReduced) or (not S`IsZBasisLLLReduced) then
     S`ZBasis:=MatrixQtoA(Algebra(S),LLL(MatrixAtoQ(ZBasis(S))));
     S`IsZBasisLLLReduced:=true;
   end if;
@@ -28,7 +28,7 @@ end intrinsic;
 
 intrinsic ZBasisLLL(S::AlgEtIdl)
 {A procedure that replaces the ZBasis with an LLL-reduced one.}
-  if not assigned S`IsZBasisLLLReduced or not S`IsZBasisLLLReduced then
+  if (not assigned S`IsZBasisLLLReduced) or (not S`IsZBasisLLLReduced) then
     S`ZBasis:=MatrixQtoA(Algebra(S),LLL(MatrixAtoQ(ZBasis(S))));
     S`IsZBasisLLLReduced:=true;
   end if;

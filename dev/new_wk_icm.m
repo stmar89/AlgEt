3389d71bcfc59e636d1b296348ade01117b0521d
Stefano Marseglia <stefano.marseglia89@gmail.com>
2023-01-20 11:15:24 -0500

    new method to compute wkicm, contained in new_wk_icm, passed tests

--- dev/new_wk_icm.m
+++ dev/new_wk_icm.m
@@ -17,7 +17,7 @@ declare verbose new_wk_icm_bar, 2;
 import "../AlgEtQ/LowCohenMacaulayType.m" : wkicm_bar_CM_type2;
 
 intrinsic IntermediateIdealsVSWithTrivialExtensionAndPrescribedMultiplicatorRing(I::AlgEtQIdl,P::AlgEtQIdl, O::AlgEtQOrd)->SetIndx[AlgEtQIdl]
-{Given a fractional S-ideal I, a prime ideal P of S, and an overorder O of S such that O subset (I:I), it returns all the fractional S-ideal J such that PI subset J subset I, (J:J) eq S, and JO=I.}
+{Given a fractional S-ideal I, a prime ideal P of S, and an overorder O of S such that O subset (I:I), it returns all the fractional S-ideals J such that PI subset J subset I, (J:J) eq S, and JO=I.}
     require IsPrime(P) : "The ideal P must be prime";
     S:=Order(P);
     require S eq Order(I) : "The ideals must be over the same order";
@@ -26,18 +26,28 @@ intrinsic IntermediateIdealsVSWithTrivialExtensionAndPrescribedMultiplicatorRing
     require O subset MultiplicatorRing(I) : "I is not an O-ideal";
     PI:=P*I;
     zbPI:=ZBasis(PI);
-    Q,q:=QuotientVS(I,P*I,P);
+    Q,q:=QuotientVS(I,P*I,P); // q:I->I/PI=Q
+    zbO:=ZBasis(O);
+
     queue:={@ Q @};
     output:={@  @};
+    if MultiplicatorRing(I) eq S then
+        Include(~output, S!!I);
+    end if;
     done:={@ @};
     while #queue gt 0 do
         pot_new:={@ x : x in &cat[ MaximalSubmodules(elt) : elt in queue ]@};
         pot_new_lifts:=[ Ideal(S,[ (Q!b)@@q : b in Basis(W) ] cat zbPI) : W in pot_new]; 
-        pot_new:={@ pot_new[i] : i in [1..#pot_new] | O!!pot_new_lifts[i] eq IO @}; //we keep only the ones with trivial extension
-        output join:={@ pot_new_lifts[i] : i in [1..#pot_new] | not pot_new[i] in done and MultiplicatorRing(pot_new_lifts[i]) eq S @};
+        indices:=[ i : i in [1..#pot_new] | O!!pot_new_lifts[i] eq IO ]; 
+            // checking trivial ext with the lifts it is faster than doing it in Q
+        pot_new:={@ pot_new[i] : i in [1..#pot_new] | i in indices @};
+            // only the ones with trivial extension
+        pot_new_lifts:={@ pot_new_lifts[i] : i in [1..#pot_new_lifts] | i in indices @};
+        output join:={@ pot_new_lifts[i] : i in [1..#pot_new] | MultiplicatorRing(pot_new_lifts[i]) eq S @};
+            // one the lifts with multiplicator ring S
         done join:=queue;
-        // Note: if O!!pot_new_lift[i] is not IO, then all the subvector spaces of pot_new[i] will also not have trivial extension IO.
-        // Hence we don't need to continue the recursion on pot_new_lift[i].
+        // If W does not have trivial extension, then all its subspace will not have trivial extension.
+        // Hence we do not add such W to the queue.
         queue := pot_new diff done; 
     end while;
     return output;
@@ -47,34 +57,32 @@ intrinsic WKICM_bar(S::AlgEtQOrd : Method:="Auto") -> SeqEnum
 {Returns all the weak eq classes I, such that (I:I)=S. The VarArg Method (default "Auto") determines if we should use the "IntermediateIdeals" routine or the "LowIndexProcess", which is potentially much slower but more memory efficient.}
     if not assigned S`WKICM_bar then
         if IsGorenstein(S) then
+            vprintf new_wk_icm_bar,2 : " Gorenstein case\n";
             S`WKICM_bar:=[OneIdeal(S)];
         elif CohenMacaulayType(S) eq 2 then
+            vprintf new_wk_icm_bar,2 : " Cohen Macaulay type 2 case\n";
             S`WKICM_bar:=wkicm_bar_CM_type2(S,NonGorensteinPrimes(S));
         else
             require Method in {"Auto","LowIndexProcess","IntermediateIdeals","IntermediateIdealsVSWithTrivialExtensionAndPrescribedMultiplicatorRing"} : "The VarArg parameter Method is assigned to a not avaialble value";
             vprintf WkClasses,2:"Order of CohenMacaulayType = %o\n",CohenMacaulayType(S);
             // general case
-            A:=Algebra(S);
-            degA:=Dimension(A);
             seqWk_bar:=[];
-            St:=TraceDualIdeal(S);
             if Method in {"Auto","IntermediateIdealsVSWithTrivialExtensionAndPrescribedMultiplicatorRing"} then
-                vprint new_wk_icm_bar,2 : "using new method";
+                vprint new_wk_icm_bar,2 : "Using new method";
                 pp:=PrimesAbove(Conductor(S));
                 oo:=FindOverOrders(S);
                 mult_pp:=[ oo[Index(oo,MultiplicatorRing(P))] : P in pp ];
                 assert forall{T : T in mult_pp | assigned T`WKICM_bar};
                 num_sub_vect_sp:=function(n,q)
                 // q a prime power. Returns the number of F_q-subvector spaces of F_q^n
-                // CHECK ME
-                    return 2+&+[ (1-q^n)*(1-q) div (1-q^k)*(1-q^(n-k)) : k in [1..n-1]];
+                    return &+[ GaussianBinomial(n,k,q) : k in [0..n]];
                 end function;
                 sub_vs_T:=[];
                 for iP->P in pp do
                   wkT:=WKICM_bar(mult_pp[iP]);
                   q:=Index(S,P);
                   dimsT:=[ Ilog(q,Index(J,(mult_pp[iP]!!P)*J)) : J in wkT ];
-                  Append(~sub_vs_T,&+[num_sub_vect_sp(q,d) : d in dimsT]);
+                  Append(~sub_vs_T,&+[num_sub_vect_sp(d,q) : d in dimsT]);
                 end for;
                 _,iP:=Min(sub_vs_T); // this is the T which make us compute less vector spaces.
                 P:=pp[iP];
@@ -88,6 +96,7 @@ intrinsic WKICM_bar(S::AlgEtQOrd : Method:="Auto") -> SeqEnum
                   end if;
                 end for;
             else
+              St:=TraceDualIdeal(S);
               T:=&meet([ T : T in FindOverOrders(S) | IsInvertible(T !! St) ]);
               //this construction of T is conjectural, hence the next assert. If the assert fails, please report it.
               assert IsInvertible(T !! St);
@@ -140,6 +149,7 @@ intrinsic WKICM_bar(S::AlgEtQOrd : Method:="Auto") -> SeqEnum
                   QP,f:=FPGroup(Q); //f:QP->Q
                   subg:=LowIndexProcess(QP,<1,#QP>);
                   while not IsEmpty(subg) do
+    "NEW method";
                       H := ExtractGroup(subg);
                       NextSubgroup(~subg);
                       I:=Ideal(S, [ (f(QP!x))@@q : x in Generators(H) ] cat gens_ff_over_S);
@@ -161,28 +171,41 @@ end intrinsic;
 
 intrinsic WKICM(E::AlgEtQOrd : Method:="Auto")->SeqEnum
 {new}
-    require Method in {"Auto","LowIndexProcess","IntermediateIdeals","IntermediateIdealsVSWithTrivialExtensionAndPrescribedMultiplicatorRing"} : "The VarArg parameter Method is assigned to a not avaialble value";
+    require Method in {"Auto","LowIndexProcess","IntermediateIdeals","IntermediateIdealsVSWithTrivialExtensionAndPrescribedMultiplicatorRing"} : "The VarArg parameter Method is assigned to a not available value";
     if not assigned E`WKICM then
         wk:=[];
         oo:=FindOverOrders(E : populateoo_in_oo:=true);
-        O:=MaximalOrder(Algebra(E));
-        edges:=[];
-        for j in [1..#oo] do
-            for i in [Index(oo,T) : T in MinimalOverOrders(oo[j])] do
-            // oo[j] subset oo[i] 
-                Append(~edges,[i,j]); //the edge is a reversed inclusion
+        if Method in {"Auto","IntermediateIdealsVSWithTrivialExtensionAndPrescribedMultiplicatorRing"} then
+            // We want to compute WKICM_bar(S) for S running from top to bottom 
+            // of the lattice of overorder of R.
+            // The reason is that when we want to compute it for S, if S is not Gorenstein or of CMType 2, then
+            // we need to have WKICM_bar(T) computed for T=(P:P) for all P singular prime of S.
+            // Hence we proceed recursively starting from O, and then filling it for the Maximal SubOrders.
+            O:=MaximalOrder(Algebra(E));
+            edges:=[];
+            for j in [1..#oo] do
+                for i in [Index(oo,T) : T in MinimalOverOrders(oo[j])] do
+                // oo[j] subset oo[i] 
+                    Append(~edges,[i,j]); //the edge is a reversed inclusion
+                end for;
             end for;
-        end for;
-        D:=Digraph<#oo|edges>;
-        vv:=VertexSet(D);
-        max_dist:=Distance(vv.Index(oo,O),vv.Index(oo,E));
-        for d in [0..max_dist] do
-            oo_d:=Sphere(vv.Index(oo,O),d);
-            oo_d:={@ oo[Index(v)] : v in oo_d @};
-            for T in oo_d do
-              wk cat:=[(E!!I) : I in WKICM_bar(T : Method:=Method)];
+            // D is the directed graph of (reverse) inclusion of overorders of R.
+            D:=Digraph<#oo|edges>;
+            vprint new_wk_icm_bar,2 : "We have computed the directed graph of (reverse) inclusion of overorders of R.";
+            vv:=VertexSet(D);
+            max_dist:=Distance(vv.Index(oo,O),vv.Index(oo,E));
+            for d in [0..max_dist] do
+                vprintf new_wk_icm_bar,2 : "Dealing now with orders of distance %o from the maximal order.\n",d;
+                oo_d:=Sphere(vv.Index(oo,O),d);
+                for v in oo_d do
+                    vprintf new_wk_icm_bar,2 : "Computing WKICM_bar for the %o-th order ... \n",v;
+                    T:=oo[Index(v)];
+                    vtime new_wk_icm_bar,2 :wk cat:=[(E!!I) : I in WKICM_bar(T : Method:=Method)];
+                end for;
             end for;
-        end for;
+        else
+            wk:=&cat[[(E!!I) : I in WKICM_bar(S : Method:=Method)] : S in oo ];
+        end if;
         for I in wk do
             ZBasisLLL(I);
         end for;
@@ -193,57 +216,87 @@ end intrinsic;
 
 
 /* TESTS
-    screen -r test_new_wk_icm
+ 
+    AttachSpec("~/packages_github/AlgEt/spec");
+    Attach("~/packages_github/AlgEt/dev/new_wk_icm.m");
+    P<x>:=PolynomialRing(Integers());
+    f:=x^8+16;
+    A:=EtaleAlgebra(f);
+    R:=EquationOrder(A);
+    time _:=FindOverOrders(R); // <4 secs
+    time assert #WKICM(R) eq 173; // ~40 secs
+    "the size of the output, 173 classes, has been computed using the OLD method below, in 189000 seconds";
+    // quit;
 
-    quit;
-    cd ~/packages_github/AlgEt/
-    git pull; sleep 1;
-    magma
+//    magma
+// 
+//     AttachSpec("~/packages_github/AlgEt/spec");
+//     "OLD method";
+//   	_<x>:=PolynomialRing(Integers());
+//     f:=x^8+16; 
+//     A:=EtaleAlgebra(f);
+//     R:=EquationOrder(A);
+//     time _:=FindOverOrders(R);
+//     time #WKICM(R);
+//     quit;
+//     screen -r test_new_wk_icm
+// 
+//     quit;
+//     cd ~/packages_github/AlgEt/
+//     git pull; sleep 1;
+//     magma
 
-    "NEW method";
+
+    // looping over slow input
     AttachSpec("~/packages_github/AlgEt/spec");
     Attach("~/packages_github/AlgEt/dev/new_wk_icm.m");
     P<x>:=PolynomialRing(Integers());
     str:=Split(Read("~/packages_github/AlgEt/dev/input_slow_sorted.txt"));
     poly:=[ P!eval(l) : l in str[1..10] ];
-    for f in poly do
-        f;
+    for i->f in poly do
         A:=EtaleAlgebra(f);
-        R:=EquationOrder(A);
-        "overorders";
-        time _:=FindOverOrders(R);
-        "wk icm";
-        time #WKICM(R);
+        F:=PrimitiveElement(A);
+        q:=Integers() ! Round(ConstantCoefficient(f)^(2/Degree(f)));
+        R:=Order([F,q/F]);
+        _:=FindOverOrders(R);
+        t0:=Cputime();
+        printf "i=%o f=%o #wk=%o seconds=%o\n",i,f,#WKICM(R),Cputime(t0);
     end for;
 
-
-
-
-
-
-
-    "NEW method";
+// ISSUE with the next: there is a bug
     AttachSpec("~/packages_github/AlgEt/spec");
     Attach("~/packages_github/AlgEt/dev/new_wk_icm.m");
+    SetVerbose("new_wk_icm_bar",2);
     P<x>:=PolynomialRing(Integers());
-    f:=x^8+16; 
+    f:=x^8 - 2*x^7 + 7*x^6 - 14*x^5 + 40*x^4 - 56*x^3 + 112*x^2 - 128*x + 256;
     A:=EtaleAlgebra(f);
+    F:=PrimitiveElement(A);
     R:=EquationOrder(A);
-    time _:=FindOverOrders(R);
-    time assert #WKICM(R) eq 173;
-    "the size of the output, 173 classes, has been computed using the OLD method below, in 189000 seconds";
-    quit;
+    "Computing overorders ...";
+    time oo:=FindOverOrders(R); //14824 seconds, 16320 overorders
+    #oo, "overorders found.";
+    for S in oo do if not IsMaximal(S) then
+        assert assigned S`MinimalOverOrders;
+    end if; end for;
+    wk:=WKICM(R);
 
-    magma
 
+// ISSUE with the next: Segmentation fault for the Equation Order. Maybe there are just too many subscpaces
     AttachSpec("~/packages_github/AlgEt/spec");
-    "OLD method";
-  	_<x>:=PolynomialRing(Integers());
-    f:=x^8+16; 
+    Attach("~/packages_github/AlgEt/dev/new_wk_icm.m");
+    SetVerbose("new_wk_icm_bar",2);
+    P<x>:=PolynomialRing(Integers());
+    f:=x^6 + 8*x^5 + 50*x^4 + 200*x^3 + 1250*x^2 + 5000*x + 15625;
     A:=EtaleAlgebra(f);
-    R:=EquationOrder(A);
-    time _:=FindOverOrders(R);
-    time #WKICM(R);
-    quit;
+    F:=PrimitiveElement(A);
+    // R:=Order([F,25/F]);
+    R:=EquationOrder(A); //3200 overorders
+    "Computing overorders ...";
+    time oo:=FindOverOrders(R);
+    #oo, "overorders found.";
+    for S in oo do if not IsMaximal(S) then
+        assert assigned S`MinimalOverOrders;
+    end if; end for;
+    wk:=WKICM(R);
 
 */
